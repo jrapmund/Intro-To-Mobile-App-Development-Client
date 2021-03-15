ba41afdd40f2d2e267e24e58ba4e85862114900e
Jacob Rapmund <jwrapmund@gmail.com>
2021-03-15 13:49:42 -0700

    updating modified files.

--- app/src/main/java/com/jrapmund/cpsc411/client/NumberCompareFeedbackFragment.kt
+++ app/src/main/java/com/jrapmund/cpsc411/client/NumberCompareFeedbackFragment.kt
@@ -1,15 +1,19 @@
 package com.jrapmund.cpsc411.client
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.github.kittinunf.fuel.Fuel
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM1 = "arg1"
 
 /**
  * A simple [Fragment] subclass.
@@ -19,13 +23,12 @@ private const val ARG_PARAM2 = "param2"
 class NumberCompareFeedbackFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            println("Got arguments $param1")
         }
     }
 
@@ -37,22 +40,36 @@ class NumberCompareFeedbackFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_number_compare_feedback, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val inputJsonString = Json.encodeToString(param1?:"0")
+        Fuel.post("http://192.168.0.176:4444/numbercompare").body(inputJsonString).response {
+                request, response, result ->
+            val (data, error) = result
+
+            if(data != null) {
+                Log.d("Web Service Log", "Data returned from REST server : ${String(data)}.")
+                view.findViewById<TextView>(R.id.txtResponse).text = String(data)
+            }
+            else Log.d("Web Service Log", "$error")
+        }
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment NumberCompareFeedbackFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String) =
             NumberCompareFeedbackFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
--- app/src/main/java/com/jrapmund/cpsc411/client/NumberCompareFragment.kt
+++ app/src/main/java/com/jrapmund/cpsc411/client/NumberCompareFragment.kt
@@ -1,33 +1,17 @@
 package com.jrapmund.cpsc411.client
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.findNavController
+import com.github.kittinunf.fuel.Fuel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NumberCompareFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NumberCompareFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,22 +21,35 @@ class NumberCompareFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_number_compare, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        println("View Created")
+        Fuel.get("http://192.168.0.176:4444/newnumber").response {
+                request, response, result ->
+            val (data, error) = result
+
+            if(data != null) {
+                Log.d("Web Service Log", "Data returned from REST server : ${String(data)}.")
+
+                val number = String(data)
+                view.findViewById<TextView>(R.id.txtActiveNumber).text = number
+            }
+            else Log.d("Web Service Log", "$error")
+        }
+
+        view.findViewById<Button>(R.id.btnSubmit).setOnClickListener{ v ->
+            val input = view.findViewById<TextView>(R.id.editTxtNumber).text?.toString()
+            println("Input is $input")
+            v.findNavController().navigate(NumberCompareFragmentDirections.actionNumberCompareFragmentToNumberCompareFeedbackFragment(input))
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NumberCompareFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             NumberCompareFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
